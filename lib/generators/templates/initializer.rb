ed69fe10f07d857a6b994f0411c77b921ab2375b
Russ Smith <russ@bashme.org>
2010-06-14 17:41:33 -0700

    Adding doc to initializer.

--- lib/generators/templates/initializer.rb
+++ lib/generators/templates/initializer.rb
@@ -1,6 +1,15 @@
 Lacquer.configure do |config|
+  # Globally enable/disable cache
   config.enable_cache = true
+
+  # Unless overridden in a controller or action, the default will be used
   config.default_ttl = 1.week
+
+  # Can be :none, :delayed_job, :resque
   config.job_backend = :none
-  config.varnish_servers << { :host => '0.0.0.0', :port => 6082 }
+
+  # Array of Varnish servers to manage
+  config.varnish_servers << { 
+    :host => '0.0.0.0', :port => 6082
+  }
 end
