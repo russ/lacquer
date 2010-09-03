258138f3c22ba6f28d9fec0121d0def08481ed0f
Garry Tan <garry@posterous.com>
2010-09-03 01:44:57 -0700

    fix tests

--- spec/lacquer/varnish_spec.rb
+++ spec/lacquer/varnish_spec.rb
@@ -6,20 +6,22 @@ describe "Varnish" do
     Net::Telnet.stub!(:new).and_return(@telnet_mock)
     @telnet_mock.stub!(:close)
     @telnet_mock.stub!(:cmd)    
+    @telnet_mock.stub!(:puts)
+    @telnet_mock.stub!(:waitfor)
     Lacquer.configuration.retries.should == 5
   end
 
   describe "with any command" do
     describe "when connection is unsuccessful" do
       it "should raise a Lacquer::VarnishError" do
-        @telnet_mock.stub!(:cmd).and_raise(Timeout::Error)
+        @telnet_mock.stub!(:waitfor).and_raise(Timeout::Error)
         lambda {
           Lacquer::Varnish.new.purge('/')
         }.should raise_error(Lacquer::VarnishError)
       end
       
       it "should retry on failure before erroring" do
-        @telnet_mock.stub!(:cmd).and_raise(Timeout::Error)
+        @telnet_mock.stub!(:waitfor).and_raise(Timeout::Error)
         Net::Telnet.should_receive(:new).exactly(5).times
         lambda {
           Lacquer::Varnish.new.purge('/')
@@ -37,7 +39,7 @@ describe "Varnish" do
         Lacquer.configuration.command_error_handler = mock("command_error_handler")
       end
       it "should call handler on error" do
-        @telnet_mock.stub!(:cmd).and_raise(Timeout::Error)
+        @telnet_mock.stub!(:waitfor).and_raise(Timeout::Error)
         Lacquer.configuration.command_error_handler.should_receive(:call).exactly(1).times
         lambda {
           Lacquer::Varnish.new.purge('/')
@@ -48,7 +50,7 @@ describe "Varnish" do
   
   describe "when sending a stats command" do
     it "should return an array of stats" do
-      @telnet_mock.stub!(:cmd).and_yield("200 200\n1000 Connections")
+      @telnet_mock.stub!(:waitfor).and_yield("200 200\n1000 Connections")
       stats = Lacquer::Varnish.new.stats
       stats.size.should be(1)
     end
@@ -56,7 +58,7 @@ describe "Varnish" do
 
   describe "when sending a purge command" do 
     it "should return successfully" do
-      @telnet_mock.stub!(:cmd).and_yield('200')
+      @telnet_mock.stub!(:waitfor).and_yield('200')
       Lacquer::Varnish.new.purge('/').should be(true)
     end
   end
