f1a98e16d042cbe0154fa5098964d7679e159da2
Jon Stenqvist <jon.stenqvist@gmail.com>
2011-01-10 00:03:06 +0100

    Added capistrano tasks for linking varnishd.yml and varnish.vcl from shared/config

--- lib/lacquer/recipes.rb
+++ lib/lacquer/recipes.rb
@@ -27,6 +27,18 @@ Capistrano::Configuration.instance.load do
       fetch(:varnishd_role, :varnish)
     end
     
+    desc "Link varnishd.yml from shared/config to app/config"
+    task :link_varnishd_yml, :roles => lambda { roles } do
+      run "cd #{release_path} && ln -nfs #{shared_path}/config/varnishd.yml  #{release_path}/config/varnishd.yml"
+    end
+    after 'deploy:update_code', 'varnishd:link_varnishd_yml'
+    
+    desc "Link varnish.vcl from shared/config to app/config"
+    task :link_varnish_vcl, :roles => lambda { roles } do
+      run "cd #{release_path} && ln -nfs #{shared_path}/config/varnish.vcl  #{release_path}/config/varnish.vcl"
+    end
+    after 'deploy:update_code', 'varnishd:link_varnishd_vcl'
+    
     desc "Purge ALL urls from Varnish"
     task :global_purge, :roles => lambda { roles } do
       run "cd #{current_path};#{rails_env} #{rake} varnishd:global_purge"
