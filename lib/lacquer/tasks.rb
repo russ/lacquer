1fd86b53a7eaabc62392ad64193719fedfa00e82
Russ Smith <russ@bashme.org>
2011-05-06 14:30:09 -0700

    Moving varnishd tasks into the lacquer namespace.

--- lib/lacquer/tasks.rb
+++ lib/lacquer/tasks.rb
@@ -1,37 +1,37 @@
-namespace :varnishd do
-  
-  desc "Start varnishd daemon using Lacquer's settings"
-  task :start => :environment do
-    Lacquer::Varnishd.new.start
-  end
-  
-  desc "Stop varnishd daemon using Lacquer's settings"
-  task :stop => :environment do
-    Lacquer::Varnishd.new.stop
-  end
-  
-  desc "Running status of varnishd daemon using Lacquer's settings"
-  task :status => :environment do
-    if Lacquer::Varnishd.new.running?
-      puts "Varnishd is running"
-    else
-      puts "Varnishd is not running"
+namespace :lacquer do
+  namespace :varnishd do
+    desc "Start varnishd daemon using Lacquer's settings"
+    task :start => :environment do
+      Lacquer::Varnishd.new.start
     end
-  end
-  
-  desc "Restart varnishd daemon using Lacquer's settings"
-  task :restart => :environment do
-    varnishd = Lacquer::Varnishd.new
-    if varnishd.running?
-      varnishd.stop
-      sleep(1)
-    end
-    varnishd.start
-  end
-  
-  desc "Purge ALL urls from Varnish"
-  task :global_purge => :environment do
-    Lacquer::Varnish.new.purge('.*')
-  end
     
+    desc "Stop varnishd daemon using Lacquer's settings"
+    task :stop => :environment do
+      Lacquer::Varnishd.new.stop
+    end
+    
+    desc "Running status of varnishd daemon using Lacquer's settings"
+    task :status => :environment do
+      if Lacquer::Varnishd.new.running?
+        puts "Varnishd is running"
+      else
+        puts "Varnishd is not running"
+      end
+    end
+    
+    desc "Restart varnishd daemon using Lacquer's settings"
+    task :restart => :environment do
+      varnishd = Lacquer::Varnishd.new
+      if varnishd.running?
+        varnishd.stop
+        sleep(1)
+      end
+      varnishd.start
+    end
+    
+    desc "Purge ALL urls from Varnish"
+    task :global_purge => :environment do
+      Lacquer::Varnish.new.purge('.*')
+    end
+  end
 end
